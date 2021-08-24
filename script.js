73c4f3a8dfb295a1eef24c4c3eacbf753ea761ac
Kevin Lee <hbl233@nyu.edu>
2021-08-24 16:32:21 -0400

    added Javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+window.alert("Kevin Lee - Web Developer Position");
+document.write("&copy; All Rights Reserved")
+
+//going to implement for responsive design
+//if width <= 900 //Name of the company && slogan disappears
+//if width <= 600 //Logo moves to the center of Header
